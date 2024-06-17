b91fc40361e6b015b3e72bcf5c442fe42477e507
赵晨炀 <904852749@qq.com>
2024-06-17 17:30:30 +0800

    feat: ai工具

---
+++ openai_flutter/lib/utils/ai_logger.dart
@@ -0,0 +1,18 @@
+import 'dart:developer' as dev;
+
+
+/// log工具
+class AiLogger {
+  static bool _isEnable = true;
+
+  static set isEnable(bool value) {
+    _isEnable = value;
+  }
+
+  static void log(String message) {
+    if(!_isEnable) {
+      return;
+    }
+    dev.log(message, name: "AILogger");
+  }
+}
